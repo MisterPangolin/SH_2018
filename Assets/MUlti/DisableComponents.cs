e2eb6a1b9fbbab69862240a5d6ab7b05bcc10243
Jerome Germain <jerome.germain@eleves.ec-nantes.fr>
2018-11-15 11:19:31 +0100

    ouverture multi joueur ...

---
+++ Assets/MUlti/DisableComponents.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DisableComponents : NetworkBehaviour {
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+	// Use this for initialization
+	void Start ()
+    {
+		if(!isLocalPlayer)
+        {
+            //boucle de désactivation des scripts ne concernant pas notre joueur
+            for(int i=0; i< componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+	}
+}
